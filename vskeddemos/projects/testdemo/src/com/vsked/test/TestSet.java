64574f8bb9ea051a99abcc0dfbd64cc3cc1a2fe0
chousheng <vsked@163.com>
2015-08-05 10:44:12 +0800

    add the test set hashset and treeset

--- vskeddemos/projects/testdemo/src/com/vsked/test/TestSet.java
+++ vskeddemos/projects/testdemo/src/com/vsked/test/TestSet.java
@@ -1,15 +1,54 @@
 package com.vsked.test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class TestSet {
 
 	public static void main(String[] args) {
-		Map tm=new HashMap();
-		tm.put("x", 1);
-		tm.put("y", 2);
-		System.out.println(tm.containsValue(3));
+//		testHashSet();
+		testTreeSet();
+	}
+
+	public static void testHashSet() {
+		HashSet h = new HashSet();
+		h.add("1st");
+		h.add("2nd");
+		h.add(new Integer(3));
+		h.add(new Double(4.0));
+		h.add("2nd"); // 重复元素，未被添加
+		h.add(new Integer(3)); // 重复元素，未被添加
+		h.add(new Date());
+		System.out.println("开始：size=" + h.size());
+		Iterator it = h.iterator();
+		while (it.hasNext()) {
+			Object o = it.next();
+			System.out.println(o);
+
+		}
+		h.remove("2nd");
+		System.out.println("移除元素后：size=" + h.size());
+		System.out.println(h);
+	}
+
+	public static void testTreeSet() {
+		TreeSet ts = new TreeSet();
+		ts.add("Aug 5");
+		ts.add("Aug 6");
+		ts.add("Aug 7");
+		ts.add("Aug 8");
+		Iterator it = ts.iterator();
+		while (it.hasNext()) {
+			String fruit = (String) it.next();
+			System.out.println(fruit);
+		}
+		for(int i=ts.size();i>0;i--){
+			Object x=ts.pollLast();
+			System.out.println(x);
+			ts.remove(x);
+		}
 	}
 
 }
