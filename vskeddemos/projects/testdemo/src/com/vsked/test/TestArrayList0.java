a2459481934880825eca59b318853f2fdb29290a
chousheng <vsked@163.com>
2015-08-06 15:22:14 +0800

    testarraylist0 again add

--- vskeddemos/projects/testdemo/src/com/vsked/test/TestArrayList0.java
+++ vskeddemos/projects/testdemo/src/com/vsked/test/TestArrayList0.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class TestArrayList0 {
 
 	public static void main(String[] args) {
+		testArrayList();
+	}
+	
+	public static void testArrayListRemove0(){
 		List<Integer> l=new ArrayList<Integer>();
 		l.add(1);
 		l.add(2);
@@ -20,6 +24,21 @@ public class TestArrayList0 {
 		l.remove(0);
 		System.out.println(l.get(0));
 		l.remove(0);
+	}
+	
+	public static void testArrayList(){
+		List<String> dateList=new ArrayList<String>();
+		dateList.add("Aug 4, 2015 9:52:06");
+		dateList.add("Aug 4, 2015 9:52:07");
+		dateList.add("Aug 4, 2015 9:52:08");
+		dateList.add("Aug 5, 2015 9:52:06");
+		dateList.add("Aug 5, 2015 9:52:07");
+		dateList.add("Aug 5, 2015 9:52:08");
+		System.out.println(dateList.contains("Aug 4, 2015 9:52:26"));
+		
+		for(String s:dateList){
+			System.out.println(s);
+		}
 	}
 
 }
