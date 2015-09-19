f294c91cf47ac1b6d3ceda5939f859e834e7400c
chousheng <vsked@163.com>
2015-09-19 11:24:16 +0800

    fixed code to jdbc4

---
+++ vskeddemos/projects/javajdbcdemo/readme.md
@@ -0,0 +1,13 @@
+windows10 64 bit
+myeclipse 2014 64bit
+jdk1.7.0_71 64 bit
+tomcat8.0.21 64bit
+oracle 12c 64bit
+
+jdbc4.1
+
+如果你使用的是dms版本驱动jar包请添加dms.jar文件到lib目录
+if use ojdbcXdms.jar please put dms.jar file to lib folder
+
+如果你数据库启动了CDB模式创建个性用户时用户名前要加c##
+if use the CDB model when create or modify or grant must add the c## front of usename
--- vskeddemos/projects/javajdbcdemo/src/com/vsked/util/ConnectOracle.java
+++ vskeddemos/projects/javajdbcdemo/src/com/vsked/util/ConnectOracle.java
@@ -15,43 +15,23 @@ public class ConnectOracle {
 	private static String userName = "brucevsked";
 	private static String userPassword = "Y4yhl9tbf110";
 	private static String dbPort="1521";
-	private static String url = "";
 
 	public static Connection getMySqlConnection() throws Exception {
-		Class.forName("oracle.jdbc.OracleDriver").newInstance();
 		conn = DriverManager.getConnection("jdbc:oracle:thin:@"+dbHost+":"+dbPort+":"+dbName, userName, userPassword);
 		return conn;
-	}
-
-	public static void closeMySqlConnection() {
-		try {
-			if (rs != null) {
-				rs.close();
-			}
-			if (pt != null) {
-				pt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
 	}
 
 	public static void main(String[] args) throws Exception {
 		Connection conn = ConnectOracle.getMySqlConnection();
 		String sql = "select * from user_tables ";
-		PreparedStatement pt = conn.prepareStatement(sql);
-		ResultSet rs=pt.executeQuery();
+		pt = conn.prepareStatement(sql);
+		rs=pt.executeQuery();
 		while(rs.next()){
 			for(int i=1;i<=rs.getMetaData().getColumnCount();i++){
 				System.out.print(rs.getString(i)+"\t");
 			}
 			System.out.println();
 		}
-		closeMySqlConnection();
 	}
 
 }
