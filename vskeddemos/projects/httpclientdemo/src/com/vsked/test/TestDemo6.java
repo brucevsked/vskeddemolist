008e3289340bc1f9bd0812bfe7a1d8fe553c0c5a
chousheng <vsked@163.com>
2016-02-29 15:26:18 +0800

    fixed testdemo6 to https connection

--- vskeddemos/projects/httpclientdemo/src/com/vsked/test/TestDemo6.java
+++ vskeddemos/projects/httpclientdemo/src/com/vsked/test/TestDemo6.java
@@ -1,70 +1,38 @@
 package com.vsked.test;
 
-import java.util.List;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.client.AbstractHttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 
 public class TestDemo6 {
-
-	public static void main(String[] args) {
-		String getPrameter = "";
-		getPrameter += "?";
-		getPrameter += "userName=admin";
-		getPrameter += "&password=admin";
-
-		String myUrl = "http://localhost:8080/hyfd/LoginController/loginCheck.html"
-				+ getPrameter;
-		// 核心应用类
-		HttpClient httpClient = HttpClients.createDefault();
-		// HTTP请求
-		HttpUriRequest request = new HttpPost(myUrl);
-		// 打印请求信息
-		System.out.println(request.getRequestLine());
-		try {
-			// 发送请求，返回响应
-			HttpResponse response = httpClient.execute(request);
-			HttpEntity entity = response.getEntity();
-			// 打印响应信息
-			System.out.println(response.getStatusLine());
-			System.out.println(EntityUtils.toString(entity));
-			
-
-			myUrl = "http://localhost:8080/hyfd/MobileInfoController/toList.html?pageSize=10";
-			request = new HttpPost(myUrl);
-			// 打印请求信息
-			System.out.println(request.getRequestLine());
-
-			request.setHeader("Cookie", getCookie(httpClient));
-			
-			response = httpClient.execute(request);
-			entity = response.getEntity();
-			// 打印响应信息
-			System.out.println(response.getStatusLine());
-			System.out.println(EntityUtils.toString(entity));
-
-		} catch (Exception e) {
-			// 协议错误
-			e.printStackTrace();
-		} 
-
-	}
 	
-	public static String getCookie(HttpClient hc){
-		StringBuilder sb=new StringBuilder();
-		//TODO fixed here
-		List<Cookie> cookies=((AbstractHttpClient)hc).getCookieStore().getCookies();
-		for(Cookie cookie:cookies){
-			sb.append(cookie.getName()+""+cookie.getValue()+";");
-		}
-		return sb.toString();
+	public static void main(String[] args) throws Exception{
+		String myUrl = "https://github.com/";
+		SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+			public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				return true;
+			}}).build();
+		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
+		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+		HttpUriRequest request = new HttpGet(myUrl);
+		System.out.println(request.getRequestLine());
+		HttpResponse response = httpClient.execute(request);
+		HttpEntity entity = response.getEntity();
+		System.out.println(response.getStatusLine());
+		System.out.println(EntityUtils.toString(entity));
+		
 	}
 
 }
