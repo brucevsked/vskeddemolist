7f53a4a39f37055d1a8f75091d6ae07f433a9d28
chousheng <vsked@163.com>
2016-04-27 08:51:21 +0800

    add jquery eachdemo

--- vskeddemos/projects/jquerydemo/js/project/demo3.js
+++ vskeddemos/projects/jquerydemo/js/project/demo3.js
@@ -1,3 +1,6 @@
 ﻿$(function(){
-	console.log('welcome jquery i am vsked');
+	var tmpArray=['a1','a2','a3','a4','a5','a6','a7','a8'];
+	$.each(tmpArray,function (myIndex,myEl){
+		console.log('|'+myIndex+'|'+myEl+'|');
+		});
 });
