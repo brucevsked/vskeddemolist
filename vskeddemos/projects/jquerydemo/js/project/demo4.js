6313511bc2ce7098244ea7123a0619caf70e2ba6
chousheng <vsked@163.com>
2016-06-11 10:15:52 +0800

    add radio and checkbox operate with jquery

--- vskeddemos/projects/jquerydemo/js/project/demo4.js
+++ vskeddemos/projects/jquerydemo/js/project/demo4.js
@@ -1,3 +1,16 @@
 ﻿$(function(){
-	console.log('welcome jquery i am vsked');
+	
 });
+
+function getSelCou(){
+	var couName=$("input[name='country']:checked").val();
+	console.log(couName);
+}
+
+function getSelC1(){
+	var myR1=[];
+	$("input[name='c1']:checked").each(function(i,o){
+		myR1.push($(o).val());
+	});
+	console.log(myR1);
+}
