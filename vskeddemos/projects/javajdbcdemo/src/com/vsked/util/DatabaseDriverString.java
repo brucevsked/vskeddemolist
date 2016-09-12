5e1b631d1b3f713076e83c1cdc3698b3ddcc63fd
chousheng <vsked@163.com>
2016-09-12 16:51:16 +0800

    fixed mysql driver to new

--- vskeddemos/projects/javajdbcdemo/src/com/vsked/util/DatabaseDriverString.java
+++ vskeddemos/projects/javajdbcdemo/src/com/vsked/util/DatabaseDriverString.java
@@ -4,7 +4,7 @@ public class DatabaseDriverString {
 	public static final String SQLSERVER2000_DRIVER="com.microsoft.jdbc.sqlserver.SQLServerDriver";
 	public static final String SQLSERVER2005_DRIVER="";
 	public static final String ORACLE_DRIVER="oracle.jdbc.OracleDriver";
-	public static final String MYSQL_DRIVER="org.gjt.mm.mysql.Driver";
+	public static final String MYSQL_DRIVER="com.mysql.jdbc.Driver";
 	public static final String DB2_DRIVER="com.ibm.db2.jdbc.app.DB2Driver";
 	public static final String ACCESS_DRIVER="sun.jdbc.odbc.JdbcOdbcDriver";
 
