0e332a9b5e12d05e5ab41e4e12f8f18e69ec3b4d
chousheng <vsked@163.com>
2016-09-23 16:20:09 +0800

    add default port

--- vskeddemos/projects/rabbitmq/rabbitmqdemo1/src/com/vsked/test/Send.java
+++ vskeddemos/projects/rabbitmq/rabbitmqdemo1/src/com/vsked/test/Send.java
@@ -1,24 +1,25 @@
 package com.vsked.test;
 
-import com.rabbitmq.client.Channel;  
-import com.rabbitmq.client.Connection;  
-import com.rabbitmq.client.ConnectionFactory;  
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
   
-public class Send {  
-    private final static String QUEUE_NAME = "hello";  
+public class Send {
+    private final static String QUEUE_NAME = "hello";
   
-    public static void main(String[] args) throws Exception {  
-        ConnectionFactory factory = new ConnectionFactory();  
-        factory.setHost("localhost");  
-        Connection connection = factory.newConnection();  
-        Channel channel = connection.createChannel();  
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
   
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);  
-        String message = "Hello World!";  
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());  
-        System.out.println(" [x] Sent '" + message + "'");  
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!we send the first message def";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
   
-        channel.close();  
-        connection.close();  
-    }  
-}  
+        channel.close();
+        connection.close();
+    }
+}
