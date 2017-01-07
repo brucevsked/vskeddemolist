f00326b216d89032488e947c109194a4c741b2e6
chousheng <vsked@163.com>
2017-01-07 15:34:07 +0800

    tomcat hot deploye test

---
+++ vskeddemos/projects/hottest2/src/com/vsked/test/UserService.java
@@ -0,0 +1,11 @@
+package com.vsked.test;
+
+public class UserService {
+	
+	public static String getName(){
+		String userName="mynameis vsked";
+		System.out.println(userName);
+		return userName;
+	}
+
+}
