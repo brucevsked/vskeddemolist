0444fcd0672856e32b85e726d23c6e0b87215f92
chousheng <vsked@163.com>
2017-06-07 16:28:43 +0800

    add basetest for poidemonew

--- vskeddemos/mavenproject/poidemonew/src/test/java/com/vsked/test/ExcelUtilTest.java
+++ vskeddemos/mavenproject/poidemonew/src/test/java/com/vsked/test/ExcelUtilTest.java
@@ -21,24 +21,11 @@ import org.junit.Test;
 
 import com.vsked.util.ExcelUtil;
 
-public class ExcelUtilTest {
+public class ExcelUtilTest extends BaseTest{
 	
-	private static Logger log = Logger.getLogger(ExcelUtilTest.class);
+	private static final Logger log = Logger.getLogger(ExcelUtilTest.class);
 	
-	@Before
-	public void initLog4j(){
-        try {
-        	String configFile=ExcelUtilTest.class.getResource("/").toString();
-        	configFile=configFile.replace("file:/", "");
-        	configFile=configFile.replace("test-classes/", "classes/"); //for maven+junit+log4j
-        	configFile+="properties/log4j.properties";
-        	PropertyConfigurator.configure(configFile);  
-        } catch (Exception ex) {  
-            System.err.println("Cannot Initialize log4j");  
-        } 
-	}
-	
-	@Test
+//	@Test
 	public void readExcel03And07(){
 		try{
 		String filePath="e:/test.xlsx";
