7e72628b718f188750e3a26bb2e3d03b35af5ee9
chousheng <vsked@163.com>
2018-04-19 11:39:53 +0800

    commonscodec sha1 encode add

--- vskeddemos/mavenproject/commonscodecdemo/src/test/java/commonscodecdemo/Md5Test.java
+++ vskeddemos/mavenproject/commonscodecdemo/src/test/java/commonscodecdemo/Md5Test.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * md5不可逆加密
+ * @author brucevsked
+ *
+ */
 public class Md5Test {
 	
 	private static final Logger log = LoggerFactory.getLogger(Md5Test.class);
---
+++ vskeddemos/mavenproject/commonscodecdemo/src/test/java/commonscodecdemo/Sha1Test.java
@@ -0,0 +1,25 @@
+package commonscodecdemo;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * sha1不可逆加密 相对md5更安全
+ * @author brucevsked
+ *
+ */
+public class Sha1Test {
+	
+	private static final Logger log = LoggerFactory.getLogger(Sha1Test.class);
+	
+	@Test
+	public void sha1Encode(){
+		String s="aaa";
+		String r=DigestUtils.sha1Hex(s.getBytes());
+		log.debug(r);
+		
+	}
+	
+}
