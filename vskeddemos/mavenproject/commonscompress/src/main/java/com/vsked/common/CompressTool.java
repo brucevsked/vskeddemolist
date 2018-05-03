8b48674a06f2af4553ae2636c98c4e5e2acf8e1a
chousheng <vsked@163.com>
2018-05-03 11:22:57 +0800

    add compresstool and junit 4 methods for tar file or folder operate

--- vskeddemos/mavenproject/commonscompress/src/main/java/com/vsked/common/CompressTool.java
+++ vskeddemos/mavenproject/commonscompress/src/main/java/com/vsked/common/CompressTool.java
@@ -4,8 +4,10 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 
 public class CompressTool {
@@ -13,7 +15,7 @@ public class CompressTool {
 	private static final int tarBuffer=1024;
 	
 	/**
-	 * 
+	 * 压缩一个文件为tar
 	 * @param sourceFile 源文件
 	 * @param targetFile 打包后文件
 	 * @throws Exception
@@ -37,5 +39,107 @@ public class CompressTool {
 		taos.closeArchiveEntry();
 		taos.close();
 	}
+	
+	/**
+	 * 解包tar文件
+	 * @param sourceFile
+	 * @param targetFile
+	 * @throws Exception
+	 */
+	public static void tarUnCompressFile(String sourceFile,String targetFile) throws Exception {
+		TarArchiveInputStream tais=new TarArchiveInputStream(new FileInputStream(sourceFile));
+		@SuppressWarnings("unused")
+		TarArchiveEntry entry = null;
+		 while ((entry = tais.getNextTarEntry()) != null){
+			 File tmpFile=new File(targetFile);
+			 OutputStream os=new FileOutputStream(tmpFile);
+			 int count=0;
+			 byte[] data=new byte[tarBuffer];
+			 while ((count = tais.read(data)) != -1){
+				 os.write(data, 0, count);
+			 }
+			 os.flush();
+			 os.close();
+		 }
+		tais.close();
+	}
+	
+	/**
+	 * 压缩一个文件夹为tar
+	 * @param sourceFolder 请以/结尾
+	 * @param targetFile
+	 * @throws Exception
+	 */
+	public static void tarCompressFolder(String sourceFolder,String targetFile) throws Exception {
+		TarArchiveOutputStream taos=new TarArchiveOutputStream(new FileOutputStream(targetFile));
+		File sourceF1=new File(sourceFolder);
+		File[] files=sourceF1.listFiles();
+		
+		String pathNew=sourceFolder.substring(0,sourceFolder.length()-1);
+		pathNew=pathNew.substring(pathNew.lastIndexOf("/")+1)+"/";
+
+		for(File file:files){
+		TarArchiveEntry tae=new TarArchiveEntry(pathNew+file.getName());
+		tae.setSize(file.length());
+		taos.putArchiveEntry(tae);
+		
+		BufferedInputStream bis=new BufferedInputStream(new FileInputStream(file));
+		int count=0;
+		byte[] data=new byte[tarBuffer];
+		
+		while((count=bis.read(data,0,tarBuffer))!=-1){
+			taos.write(data,0,count);
+		}
+		
+		bis.close();
+		taos.closeArchiveEntry();
+		}
+		taos.close();
+	}
+	
+	/**
+	 * 解压包括一层文件夹的tar文件
+	 * @param sourceFile 源tar文件
+	 * @param targetFolder 请以/结尾
+	 * @throws Exception
+	 */
+	public static void tarUnCompressFolder(String sourceFile,String targetFolder) throws Exception {
+		TarArchiveInputStream tais=new TarArchiveInputStream(new FileInputStream(sourceFile));
+		TarArchiveEntry entry = null;
+		 while ((entry = tais.getNextTarEntry()) != null){
+			 String entryName=entry.getName();
+			 if(entryName.contains("/")){
+				 String folderName=entryName.substring(0,entryName.indexOf("/"));
+				 createDir(targetFolder+folderName);
+			 }
+			 File tmpFile=new File(targetFolder+entry.getName());
+			 OutputStream os=new FileOutputStream(tmpFile);
+			 int count=0;
+			 byte[] data=new byte[tarBuffer];
+			 while ((count = tais.read(data)) != -1){
+				 os.write(data, 0, count);
+			 }
+			 os.flush();
+			 os.close();
+			
+		 }
+		tais.close();
+	}
+	
+	/**
+	 * 根据传入的路径创建目录如果存在则不创建
+	 * @param destDirName 要创建的目录路径
+	 * @return true创建目录成功 false 创建目录失败
+	 */
+    public static boolean createDir(String destDirName) {
+        File dir = new File(destDirName);
+        if (dir.exists()) {
+            return false;
+        }
+        if (!destDirName.endsWith(File.separator)) {
+            destDirName = destDirName + File.separator;
+        }
+        return dir.mkdirs();
+    }
 
 }
--- vskeddemos/mavenproject/commonscompress/src/test/java/com/vsked/common/CompressToolTest.java
+++ vskeddemos/mavenproject/commonscompress/src/test/java/com/vsked/common/CompressToolTest.java
@@ -8,7 +8,7 @@ public class CompressToolTest {
 	
 	private static final Logger log=LoggerFactory.getLogger(CompressToolTest.class);
 	
-	@Test
+//	@Test
 	public void tarCompress(){
 		try{
 			String sourceFile="d:/source.txt";
@@ -19,6 +19,54 @@ public class CompressToolTest {
 			e.printStackTrace();
 			log.error(e.getMessage());
 		}
+	}
+	
+//	@Test
+	public void tarUnCompressFile(){
+		try{
+			String sourceFile="d:/testv1.tar";
+			String targetFile="d:/ssta1.txt";
+			CompressTool.tarUnCompressFile(sourceFile, targetFile);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			log.error(e.getMessage());
+		}
+	}
+	
+//	@Test
+	public void tarCompressFolder(){
+		try{
+			String sourceFile="D:/logs/zhongyousj/";
+			String targetFile="d:/logs/testv1.tar";
+			CompressTool.tarCompressFolder(sourceFile, targetFile);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			log.error(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void tarUnCompressFolder(){
+		try{
+			String sourceFile="d:/logs/testv1.tar";
+			String targetFolder="d:/logs/t1/";
+			CompressTool.tarUnCompressFolder(sourceFile, targetFolder);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			log.error(e.getMessage());
+		}
+	}
+	
+//	@Test
+	public void t(){
+		String s="D:/logs/zhongyousj/";
+		s=s.substring(0,s.length()-1);
+		log.debug("a:"+s);
+		s=s.substring(s.lastIndexOf("/")+1)+"/";
+		log.debug(s);
 	}
 	
 }
