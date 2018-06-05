c5d9cb862cc57f2399b44824967e5159502dcb06
chousheng <vsked@163.com>
2018-06-05 10:45:23 +0800

    fixed TraceUtils,add method getTraceId

--- vskeddemos/mavenproject/slf4jdemo/src/main/java/com/vsked/common/TraceUtils.java
+++ vskeddemos/mavenproject/slf4jdemo/src/main/java/com/vsked/common/TraceUtils.java
@@ -5,26 +5,31 @@ import org.slf4j.MDC;
 
 public class TraceUtils {
 	
-    public static final String TRACE_KEY = "mdc_trace_id";  
+    public static final String TRACE_KEY = "mdc_trace_id";
     public static final int TRACE_LENGTH = 16;
-    
-    public TraceUtils(){  
-    }  
-  
-    public static void beginTrace(){  
-        String traceId = RandomStringUtils.randomAlphanumeric(TRACE_LENGTH);  
-        MDC.put(TRACE_KEY, traceId);  
-    }  
-  
-    public static void beginTrace(String traceId){  
-        MDC.put(TRACE_KEY, traceId);  
-    }  
-  
-    public static void endTrace(){  
-        MDC.remove(TRACE_KEY);  
-    }  
-  
-    public static String getTraceIdKey(){  
-        return (String)MDC.get(TRACE_KEY);  
-    }  
+
+    public TraceUtils(){
+    }
+
+    public static void beginTrace(){
+        String traceId = RandomStringUtils.randomAlphanumeric(TRACE_LENGTH);
+        MDC.put(TRACE_KEY, traceId);
+    }
+
+    public static String getTraceId(){
+    	String traceId = RandomStringUtils.randomAlphanumeric(TRACE_LENGTH);
+    	return traceId;
+    }
+
+    public static void beginTrace(String traceId){
+        MDC.put(TRACE_KEY, traceId);
+    }
+
+    public static void endTrace(){
+        MDC.remove(TRACE_KEY);
+    }
+
+    public static String getTraceIdKey(){
+        return (String)MDC.get(TRACE_KEY);
+    }
 }
