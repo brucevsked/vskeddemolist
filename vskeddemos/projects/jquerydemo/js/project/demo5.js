35d5b5012b44c0c1d11a4ab35565dceecbcf8f2e
chousheng <vsked@163.com>
2018-09-06 14:34:33 +0800

    jquery selected value

--- vskeddemos/projects/jquerydemo/js/project/demo5.js
+++ vskeddemos/projects/jquerydemo/js/project/demo5.js
@@ -1,3 +1,15 @@
-﻿$(function(){
-	console.log('welcome jquery i am vsked');
+﻿'use strict'
+
+
+$(function(){
+	$('#testBt').on('click',demo5.test1);
+});
+
+
+var demo5=new Object({
+	test1:function(){
+		var t1=$("#playTypeSel").find("option:selected").text(); //获取Select选择的text
+		var v1=$("#playTypeSel").val(); //获取Select选择的Value
+		console.log(t1+'|'+v1)
+	}
 });
