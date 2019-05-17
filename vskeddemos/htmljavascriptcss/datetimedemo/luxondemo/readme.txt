c49484d3112c2b970f23a66bf4c1e1f3c5f835a8
chousheng <vsked@163.com>
2019-05-17 14:56:19 +0800

    update commonlang3 version

--- vskeddemos/htmljavascriptcss/datetimedemo/luxondemo/readme.txt
+++ vskeddemos/htmljavascriptcss/datetimedemo/luxondemo/readme.txt
@@ -1,3 +1,10 @@
 ﻿日期时间操作js
 https://moment.github.io/luxon/
+
+
+下载地址
+https://moment.github.io/luxon/
+源码地址
+https://github.com/moment/luxon
+api地址
 https://moment.github.io/luxon/docs/manual/install.html
---
+++ vskeddemos/mavenproject/commonlangdemo/src/main/java/com/vsked/test/GenerateTest.java
@@ -0,0 +1,26 @@
+package com.vsked.test;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenerateTest {
+	
+	private static final Logger log = LoggerFactory.getLogger(GenerateTest.class);
+	
+	@Test
+	public void generateRandomStringUtils(){
+		String num=RandomStringUtils.randomAlphanumeric(20);//随机字符将从拉丁字母（a-z、A-Z）和数字0-9中选择
+		log.debug("randomAlphanumeric|"+num+"|");
+		num=RandomStringUtils.randomAlphabetic(20);//随机字符将从拉丁字母（a-z、A-Z的选择
+		log.debug("randomAlphabetic|"+num+"|");
+		num=RandomStringUtils.randomNumeric(20);//随机数字
+		log.debug("randomNumeric|"+num+"|");
+		num=RandomStringUtils.randomAscii(15);//从ASCII 32到126组成的随机字符串
+		log.debug("randomAscii|"+num+"|");
+		num=RandomStringUtils.randomGraph(20);//数字字母大小写特殊符号
+		log.debug("randomGraph|"+num+"|");
+	}
+
+}
