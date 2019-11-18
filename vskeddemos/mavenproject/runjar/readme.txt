7e67ec302e1069d344c4b940b18f3ce7dc1b73fe
chousheng <vsked@163.com>
2019-11-18 13:16:02 +0800

    add run jar for maven

---
+++ vskeddemos/mavenproject/runjar/readme.txt
@@ -0,0 +1,9 @@
+
+maven打包可运行的jar包
+
+maven clean
+maven package
+
+参考
+https://blog.csdn.net/silentwolfyh/article/details/81506977
+
---
+++ vskeddemos/mavenproject/runjar/src/main/java/com/vsked/test/TestRunJar.java
@@ -0,0 +1,14 @@
+package com.vsked.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestRunJar {
+	
+	private static final Logger log = LoggerFactory.getLogger(TestRunJar.class);
+
+	public static void main(String[] args) {
+		log.debug("my programs has been run ...");
+	}
+
+}
