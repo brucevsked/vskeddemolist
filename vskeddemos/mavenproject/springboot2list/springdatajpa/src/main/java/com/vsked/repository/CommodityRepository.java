764dacd1da96dc3ecfa518cb3e82901de3393b17
bruce <vsked@163.com>
2020-06-28 14:57:10 +0800

    update spring data jpa

--- vskeddemos/mavenproject/springboot2list/springdatajpa/src/main/java/com/vsked/repository/CommodityRepository.java
+++ vskeddemos/mavenproject/springboot2list/springdatajpa/src/main/java/com/vsked/repository/CommodityRepository.java
@@ -1,9 +1,10 @@
 package com.vsked.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.vsked.entity.Commodity;
 
-public interface CommodityRepository extends JpaRepository<Commodity, Integer>{
+public interface CommodityRepository extends JpaRepository<Commodity, Integer>,JpaSpecificationExecutor<Commodity>{
 	
 }
--- vskeddemos/mavenproject/springboot2list/springdatajpa/src/test/java/com/vsked/test/CommodityRepositoryTest.java
+++ vskeddemos/mavenproject/springboot2list/springdatajpa/src/test/java/com/vsked/test/CommodityRepositoryTest.java
@@ -1,35 +1,46 @@
 package com.vsked.test;
 
-import java.math.BigDecimal;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 
 import com.vsked.entity.Commodity;
 import com.vsked.repository.CommodityRepository;
+import com.vsked.repository.CommofitySpecificationByName;
 
 public class CommodityRepositoryTest extends BaseTest{
 	
 	private static final Logger log = LoggerFactory.getLogger(CommodityRepositoryTest.class);
 	
 	@Autowired
-	CommodityRepository CommodityRepository;
-	
-	@Test
-	public void test1() {
-		Commodity commodity=new Commodity(1, "apple", new BigDecimal(15.52));
-		CommodityRepository.save(commodity);
-		log.debug("save ok");
-		
-	}
+	CommodityRepository commodityRepository;
 	
 //	@Test
+	public void test1() {
+		String myName="apple";
+		Specification<Commodity> specification = new Specification<Commodity>() {
+			private static final long serialVersionUID = 1L;
+			public Predicate toPredicate(Root<Commodity> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+			    return criteriaBuilder.like(root.get("name"), myName);
+			   }
+			  };
+	    Commodity commodity=commodityRepository.findOne(specification).get();
+		log.debug("query ok"+commodity.getPrice());
+	}
+	
+	@Test
 	public void test2() {
-		Integer myid=new Integer(1);
-		Commodity myCommodity=CommodityRepository.findById(myid).get();
-		log.debug("query ok"+myCommodity);
+		String myName="apple";
+		Specification<Commodity> specification=new CommofitySpecificationByName(myName);
+	    Commodity commodity=commodityRepository.findOne(specification).get();
+		log.debug("query ok"+commodity.getPrice());
 	}
 	
 }
