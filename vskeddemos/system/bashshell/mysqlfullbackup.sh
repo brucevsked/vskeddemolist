d3fbe22c0fde2d30c7636ffffc4b9d078866e33c
bruce <vsked@163.com>
2020-08-13 11:19:49 +0800

    add mysql backup script

---
+++ vskeddemos/system/bashshell/mysqlfullbackup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# now time 20200813
+dateNow=$(date "+%Y%m%d")
+
+mysqlUserName=root
+mysqlUserPass=root123456
+backUpDatabaseName=ry
+# my socket path
+mysqlSocket=/data/mysql/mysql/mysql.sock
+#default socket path
+# mysqlSocket=/var/lib/mysql/mysql.sock
+
+fullBackUpFolderBase=/data/backup/mysql/fullbackup
+fullBackUpFolder="${fullBackUpFolderBase}/${dateNow}"
+
+mkdir -p ${fullBackUpFolder}
+
+fullBackUpLog="${fullBackUpFolderBase}/mysqlFullBackUp.log"
+
+echo "start full back up:$(date "+%Y-%m-%d %H:%M:%S")" >> ${fullBackUpLog}
+echo $(date) >> ${fullBackUpLog}
+
+backUpFileNameBase="$(date "+%Y%m%d%H%M%S")fullback.sql"
+backUpFileName="${fullBackUpFolder}/${backUpFileNameBase}"
+mysqldump --socket=${mysqlSocket} -u${mysqlUserName} -p${mysqlUserPass} -B ${backUpDatabaseName} > ${backUpFileName}
+
+cd ${fullBackUpFolder}
+tar -zcvf ${backUpFileName}.tar.gz ${backUpFileNameBase}
+rm -rf ${backUpFileName}
+echo "finish full back up:$(date "+%Y-%m-%d %H:%M:%S")" >> ${fullBackUpLog}
