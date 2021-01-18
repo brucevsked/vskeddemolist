aa5fb349a7625936ed79cddc30428e7d9ac0b7e5
bruce <vsked@163.com>
2021-01-18 16:36:06 +0800

    add summernote and jstree

--- vskeddemos/mavenproject/commonlangdemo/src/test/java/com/vsked/test/HtmlEscapeTest.java
+++ vskeddemos/mavenproject/commonlangdemo/src/test/java/com/vsked/test/HtmlEscapeTest.java
@@ -1,10 +1,16 @@
 package com.vsked.test;
 
 import org.apache.commons.lang3.StringEscapeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
 
 public class HtmlEscapeTest {
 
-	public static void main(String[] args) {
+	private static final Logger log = LoggerFactory.getLogger(HtmlEscapeTest.class);
+
+	@Test
+	public void oldHtmlEscape(){
 		String s1="";
 		s1+="<html>";
 		s1+="<head>";
@@ -16,14 +22,37 @@ public class HtmlEscapeTest {
 		s1+="<script>function t1(){ console.log(1) }</script>";
 		s1+="</body>";
 		s1+="</html>";
-		
-		String result=StringEscapeUtils.escapeHtml4(s1);
-		//html
-		System.out.println(result);
-		
-		String revResult=StringEscapeUtils.unescapeHtml4(result);
-		System.out.println(revResult);
 
+		String result=StringEscapeUtils.escapeHtml4(s1);
+		//html 已经过时 需要更新为common-text包中org.apache.commons.text.StringEscapeUtils
+		log.debug(result);
+
+		String revResult=StringEscapeUtils.unescapeHtml4(result);
+		log.debug(revResult);
 	}
+
+	@Test
+	public void htmlEscape(){
+		String s1="";
+		s1+="<html>";
+		s1+="<head>";
+		s1+="</head>";
+		s1+="<body>";
+		s1+="hello this is myc1";
+		s1+="<hr>";
+		s1+="<font size=10>s1</font>";
+		s1+="<script>function t1(){ console.log(1) }</script>";
+		s1+="</body>";
+		s1+="</html>";
+
+		//新版本用法
+		String result=org.apache.commons.text.StringEscapeUtils.escapeHtml4(s1);
+		log.debug(result);
+
+		String revResult=org.apache.commons.text.StringEscapeUtils.unescapeHtml4(result);
+		log.debug(revResult);
+	}
+
+
 
 }
