ef9cc348f0733a28abe376dc9fe4a114ae9aca8b
bruce <vsked@163.com>
2021-03-08 17:22:45 +0800

    Update envchange.sh

--- vskeddemos/system/bashshell/envchange.sh
+++ vskeddemos/system/bashshell/envchange.sh
@@ -8,13 +8,30 @@ echo 默认为开发环境dev,根据传参会选择test测试环境，prod生产
 
 curenv=$1
 
-if [ ! $curenv || $curenv=dev ]; then
-  echo "未传入参数，默认为开发环境"
-  curenv=dev
-  elif [  $curenv=dev ]; then
-  echo 
-else
-  echo "当前环境参数不能识别"
-  echo $curenv
-fi
+
+case $curenv in
+    "dev" | "")
+    echo "未传入参数，默认为开发环境"
+    cp dev/application.yml /data/svn/project/mallrule/src/main/resources/application.yml
+    cp dev/application-dev.yml /data/svn/project/mallrule/src/main/resources/application-dev.yml
+    cp dev/log4j2.xml /data/svn/project/mallrule/src/main/resources/log4j2.xml
+    cp dev/webCommon.js /data/svn/project/mallrule/src/main/resources/static/js/lib/common/webCommon.js
+    ;;
+    "test")
+    echo "当前为测试环境啦"
+    cp test/application.yml /data/svn/project/mallrule/src/main/resources/application.yml
+    cp test/application-test.yml /data/svn/project/mallrule/src/main/resources/application-test.yml
+    cp test/log4j2.xml /data/svn/project/mallrule/src/main/resources/log4j2.xml
+    cp test/webCommon.js /data/svn/project/mallrule/src/main/resources/static/js/lib/common/webCommon.js
+    ;;
+    "prod")
+    echo "当前为生产环境呀"
+    cp prod/application.yml /data/svn/project/mallrule/src/main/resources/application.yml
+    cp prod/application-prod.yml /data/svn/project/mallrule/src/main/resources/application-prod.yml
+    cp prod/log4j2.xml /data/svn/project/mallrule/src/main/resources/log4j2.xml
+    cp prod/webCommon.js /data/svn/project/mallrule/src/main/resources/static/js/lib/common/webCommon.js
+    ;;
+esac
+
+
 
