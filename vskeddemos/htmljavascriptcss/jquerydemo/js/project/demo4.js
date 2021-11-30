7300ebb34cb0d7458a065127dc189e1bd514f0ca
bruce <vsked@163.com>
2021-11-30 14:55:12 +0800

    Update demo4.js

--- vskeddemos/htmljavascriptcss/jquerydemo/js/project/demo4.js
+++ vskeddemos/htmljavascriptcss/jquerydemo/js/project/demo4.js
@@ -9,8 +9,12 @@ function getSelCou(){
 
 function getSelC1(){
 	var myR1=[];
-	$("input[name='c1']:checked").each(function(i,o){
-		myR1.push($(o).val());
+	var checkBoxList=$("input[name='c1']:checked");
+	$.each(checkBoxList,function(myIndex,myEl){
+		// var curCk=$(myEl);
+		// console.log(curCk.val()+"|"+curCk.attr("id")); //方案1
+		myR1.push(myEl.value);
+		console.log(myEl.value+"|"+myEl.id); //方案2
 	});
 	console.log(myR1);
 }
