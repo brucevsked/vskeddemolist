b25546473ebe96de3b07995a55f1e7ba726a94d7
bruce <vsked@163.com>
2022-03-17 17:34:42 +0800

    add lesson3 axios
    axios test

---
+++ vskeddemos/vue3lesson/lesson3/README.md
@@ -0,0 +1,39 @@
+vite+vue3+axios
+使用路由跳转页面
+
+cd L:\git\vskeddemolist\vskeddemos\vue3lesson
+npm create vite@latest
+输入项目名称
+lesson3
+选择项目类型
+vue
+cd L:\git\vskeddemolist\vskeddemos\vue3lesson\lesson3
+npm install
+
+
+使用vscode打开文件夹lesson3
+然后打开package.json选择scripts上调试即可运行当前项目
+运行起来后访问下面地址即可
+http://localhost:3000/
+
+也可以使用命令方式
+cd L:\git\vskeddemolist\vskeddemos\vue3lesson\lesson3
+npm run dev
+运行起来后访问下面地址即可
+http://localhost:3000/
+
+
+修改package.json添加依赖 axios
+npm info axios versions
+安装
+npm install axios
+这时候就会看到package.json中多了一句
+    "axios": "^0.26.1",
+说明安装成功
+
+安装vue3与vite
+npm install
+
+
+
+
---
+++ vskeddemos/vue3lesson/lesson3/src/App.vue
@@ -0,0 +1,48 @@
+
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <button @click="get1">get异步</button> &nbsp;&nbsp;
+  <button @click="get4">get同步</button> &nbsp;&nbsp;
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+<script>
+import axios from "axios";
+export default {
+  setup() {},
+  methods: {
+    get1() {
+      console.log("----------------get异步");
+      axios
+        .get("http://localhost:3000/index.html")
+        .then(function (res) {
+          console.log(res);
+        })
+        .catch(function (error) {
+          // 请求失败处理
+          console.log(error);
+        });
+    },
+    get2() {
+      console.log("----------------get同步1");
+    },
+    get3() {
+      console.log("----------------get同步2");
+    },
+    get4() {
+      console.log("----------------get同步3");
+    },
+  },
+};
+</script>
+
