6b1ae4151ab80ebf6653da3fa56185818464cbd3
bruce <vsked@163.com>
2022-03-22 16:10:54 +0800

    update kafka demo

--- vskeddemos/mavenproject/kafkatest/src/test/java/com/vsked/test/Consume1Test.java
+++ vskeddemos/mavenproject/kafkatest/src/test/java/com/vsked/test/Consume1Test.java
@@ -11,46 +11,46 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.header.Header;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
 
 public class Consume1Test {
-	
-	private final Logger log = LoggerFactory.getLogger(Consume1Test.class);
-	
-	@Test
-	public void t1(){
+
+    private final Logger log = LoggerFactory.getLogger(Consume1Test.class);
+
+    @Test
+    public void t1() {
         Properties p = new Properties();
-        p.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.60.10:9092");
+        p.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.0.193.11:9092");
         p.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         p.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         p.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
         p.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         p.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
 
-        try(KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(p)){
-        kafkaConsumer.subscribe(Collections.singletonList(Producer1Test.topic));// 订阅消息
-        
-        Header data1=null;
-        Iterator<Header> it=null;
+        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(p)) {
+            kafkaConsumer.subscribe(Collections.singletonList(Producer1Test.topic));// 订阅消息
 
-//        while (true) {
-            ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(100));
-            for (ConsumerRecord<String, String> record : records) {
+            Header data1 = null;
+            Iterator<Header> it = null;
+
+//            while (true) {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(100));
+                for (ConsumerRecord<String, String> record : records) {
 //                log.info(record.value()+"|"+record.timestamp()+"|"+record.partition());
-                log.info(record.value()+"|"+record.key()+"|"+record.headers());
-                it =record.headers().iterator();
-                
-                while(it.hasNext()){
-                	data1=it.next();
-                	log.info(data1.key()+"|"+new String(data1.value()));
+                    log.info(record.value() + "|" + record.key() + "|" + record.headers());
+                    it = record.headers().iterator();
+
+                    while (it.hasNext()) {
+                        data1 = it.next();
+                        log.info(data1.key() + "|" + new String(data1.value()));
+                    }
                 }
-            }
-//        }
-        }catch(Exception e){
-        	log.error(e.getMessage(), e);
+//            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
-	}
+    }
 
 }
--- vskeddemos/mavenproject/kafkatest/src/test/java/com/vsked/test/Producer1Test.java
+++ vskeddemos/mavenproject/kafkatest/src/test/java/com/vsked/test/Producer1Test.java
@@ -2,14 +2,13 @@ package com.vsked.test;
 
 import java.util.Properties;
 import java.util.Random;
-
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
 
 public class Producer1Test {
 	
@@ -21,7 +20,7 @@ public class Producer1Test {
 	public void t1() {
 		
 		Properties p = new Properties();
-		p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.60.10:9092");// kafka地址，多个地址用逗号分割
+		p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.0.193.11:9092");// kafka地址，多个地址用逗号分割
 		p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
@@ -29,7 +28,7 @@ public class Producer1Test {
 //			while (true) {
 				String msg = "Hello 这里是我发的消息," + new Random().nextInt(100);
 				ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, msg);
-				record.headers().add("filename", "yellobook.txt".getBytes());
+				//record.headers().add("filename", "yellobook.txt".getBytes());
 				kafkaProducer.send(record);
 				log.info("消息发送成功hallo11:" + msg);
 				Thread.sleep(1500);
