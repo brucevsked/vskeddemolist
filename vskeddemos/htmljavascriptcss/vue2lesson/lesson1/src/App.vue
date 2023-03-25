26239792b093c62511ba46debc9977e5631e26f1
bruce <vsked@163.com>
2023-03-25 11:43:42 +0800

    add vue2 manual and update vue2demo
    update vue2 give parameter to another page
    add vskedvue2 file

--- vskeddemos/htmljavascriptcss/vue2lesson/lesson1/src/App.vue
+++ vskeddemos/htmljavascriptcss/vue2lesson/lesson1/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png"><br>
+    <router-link to="/helloworldPage/ab123方案1">method1路由导航</router-link> &nbsp;&nbsp;
+    <button @click="giveValue('方案2cd')">push方案</button> &nbsp;&nbsp;
+    <button @click="giveValueByName('方案3ef')">路径名配合params</button> &nbsp;&nbsp;
+    <router-link :to="{path:'/helloworldPage',query:{p1:'汉语url9999'}}">GivePar</router-link> &nbsp;&nbsp;
+    <button @click="giveValueByQuery('方案5gh')">query配合脚本</button> &nbsp;&nbsp;
 	<router-link to="/educationPage">click here</router-link>
 	<router-view></router-view>
   </div>
@@ -10,7 +15,31 @@
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  methods:{
+      giveValue(par2){
+          this.$router.push({
+              path:"/helloworldPage/"+par2
+          });
+      },
+      giveValueByName(par1){
+          this.$router.push({
+              name:"helloworldPage",
+              params:{
+                par1:par1
+              }
+          });
+      },
+      giveValueByQuery(par1){
+          this.$router.push({
+              path:"/helloworldPage/",
+              query:{
+                p1:par1
+              }
+          });
+      }
+      
+  }
 }
 </script>
 
--- vskeddemos/htmljavascriptcss/vue2lesson/lesson1/src/router/index.js
+++ vskeddemos/htmljavascriptcss/vue2lesson/lesson1/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import educationPage from '../components/educationPage'
+import helloworldPage from '../components/HelloWorld'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/educationPage',
       name: 'educationPage',
       component: educationPage
+    },
+    {
+      path: '/helloworldPage',
+      name: 'helloworldPage',
+      component: helloworldPage
     }
   ]
 });
