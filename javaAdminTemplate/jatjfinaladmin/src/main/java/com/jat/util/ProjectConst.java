8b5593f4e455a709ac182e819a6c173a490b168e
bruce <vsked@163.com>
2025-03-28 14:30:29 +0800

    update version

--- javaAdminTemplate/jatjfinaladmin/src/main/java/com/jat/util/ProjectConst.java
+++ javaAdminTemplate/jatjfinaladmin/src/main/java/com/jat/util/ProjectConst.java
@@ -1,6 +1,6 @@
 package com.jat.util;
 
 public class ProjectConst {
-    public static String APP_ID="wx87ee89d14d9d3d1d"; //君安泰科技小程序
-    public static String APPSECRET="9e4eabc50a655211854ee451095d1c8b";
+    public static String APP_ID=""; //君安泰科技小程序
+    public static String APPSECRET="";
 }
