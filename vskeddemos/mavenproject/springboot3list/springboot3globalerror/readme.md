5c5d3412bfb82b51815e8c3ff29c0855d88fe381
bruce <vsked@163.com>
2025-04-28 09:31:51 +0800

    move spring boot3 demo to list

---
+++ vskeddemos/mavenproject/springboot3list/springboot3globalerror/readme.md
@@ -0,0 +1,17 @@
+
+# spring boot 3 全局异常处理
+
+单元测试时继续下面这个类是带事务管理的  
+AbstractTransactionalTestNGSpringContextTests  
+
+下面这个不带事务管理的  
+AbstractTestNGSpringContextTests  
+
+http://127.0.0.1:8181
+
+http://127.0.0.1:8181/test2
+测试这个地址
+http://127.0.0.1:8181/test3
+
+http://127.0.0.1/prod-api/profile/images/tiding.png
+
---
+++ vskeddemos/mavenproject/springboot3list/springboot3globalerror/src/main/java/com/vsked/auth/web/TestController.java
@@ -0,0 +1,63 @@
+package com.vsked.auth.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.vsked.auth.service.AccountService;
+import com.vsked.auth.web.model.LoginInfoVO;
+
+@RestController
+public class TestController {
+	
+	private static final Logger log = LoggerFactory.getLogger(TestController.class);
+    	
+	@Autowired
+    AccountService accountService;
+	
+	@PostMapping("/test")
+    @ResponseBody
+    public String test1(@ModelAttribute LoginInfoVO loginInfo){
+        if(log.isTraceEnabled()){
+            log.trace("start");
+        }
+
+        String accountId=accountService.getAccountId();
+
+        if(log.isTraceEnabled()){
+            log.trace("end");
+        }
+
+        return accountId;
+    }
+
+    @GetMapping("/test/{ida}")
+    @ResponseBody
+    public String test3(@PathVariable("ida") String id,@RequestParam("username") String username,@RequestParam("password") String password){
+        if(log.isTraceEnabled()){
+            log.trace("start");
+        }
+
+        if(log.isInfoEnabled()){
+            log.info(id+"testok"+username+password);
+        }
+
+        return id+"testok"+username+password;
+    }
+
+    @GetMapping("/test2")
+    @ResponseBody
+    public String test2(){
+        return "test2ok";
+    }
+
+    @GetMapping("/test3")
+    @ResponseBody
+    public String test3(){
+        if(log.isTraceEnabled()){
+            log.trace("test 3 ");
+        }
+        throw new IllegalArgumentException("用户名不能为空字符串！");
+    }
+
+}
